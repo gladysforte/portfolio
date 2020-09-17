6a903f96608b5cc75b06440bb09f8822e55f8afc
gladys <gladys@tirsolutions.com>
2020-09-18 01:13:23 +0800

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,28 @@ function myFunction() {
     document.getElementById("about").style.display = "block";
     document.getElementById("home").style.display = "none";
 }
+  
+$('#contact-form').submit(function(e) {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('_replyto').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+
+    if (!name || !email || !message || !subject) {
+        $(".alert-success").hide();
+        $(".alert-danger").show();
+    } else {
+        $.ajax({
+            url: "https://formspree.io/xvovplrn",
+            method: "POST",
+            data: $(this).serialize(),
+            dataType: "json"
+        });
+
+        e.preventDefault();
+        $(this).get(0).reset();
+        $(".alert-danger").hide();
+        $(".alert-success").show();
+    }
+})
+
