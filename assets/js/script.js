55d839f3b3d9f96c2c5f53efbec115c242da4474
gladys <gladys@tirsolutions.com>
2020-09-21 01:34:39 +0800

    design updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,5 +62,24 @@ $('#contact-form').submit(function(e) {
 //       }
 
 
+var sizeTheOverlays = function() {
+    $(".overlay").resize().each(function() {
+    var h = $(this).parent().outerHeight();
+    var w = $(this).parent().outerWidth();
+    $(this).css("height", h);
+    $(this).css("width", w);
+  });
+};
+
+sizeTheOverlays();
+
+
+var width = $(window).width();
+$(window).resize(function(){
+   if($(this).width() != width){
+      width = $(this).width();
+      sizeTheOverlays();
+   }
+});
 
 
